d5bc73d87e3797f4005e7f894a515ac5597d20f6
DanPasqua-CF <dan.pasqua.cf@gmail.com>
2025-11-25 22:12:26 -0500

    Complete Exercise 1.4

---
+++ Exercise-1.5/Practice Tasks/shopping_list.py
@@ -0,0 +1,36 @@
+class ShoppingList(object):
+    def __init__(self, list_name):
+        self.list_name = list_name
+        self.shopping_list = []
+
+    def add_item(self, item):
+        if item not in self.shopping_list:
+            self.shopping_list.append(item)
+
+    def remove_item(self, item):
+        if item in self.shopping_list:
+            self.shopping_list.remove(item)
+
+    def view_list(self):
+        print(f"Shopping list: {self.list_name}")
+
+        for item in self.shopping_list:
+            print(f"- {item}")
+
+pet_store_list = ShoppingList("Pet Store Shopping List")
+
+# Add items
+pet_store_list.add_item("dog food")
+pet_store_list.add_item("frisbee")
+pet_store_list.add_item("bowl")
+pet_store_list.add_item("collars")
+pet_store_list.add_item("flea collars")
+
+# Remove item
+# pet_store_list.remove_item("flea collars")
+
+# Add item
+pet_store_list.add_item("frisbee")
+
+# View list
+pet_store_list.view_list()
